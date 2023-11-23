4de24f4f930ac4fa495beab1e8aa3db42756ea4e
Daniel <denamudu@bariansystems.com>
2023-11-23 09:30:08 +0100

    Initial commit.

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+     
+      var owl = $("#fd2");
+	  
+     
+      owl.owlCarousel({
+         
+          itemsCustom : [
+            [0, 1],
+			[360, 1],
+            [450, 2],
+            [600, 3],
+            [768, 3],
+            [1000, 4],
+            [1200, 4],
+            [1400, 4],
+            [1600, 4]
+          ],
+          navigation : true
+     
+      });
+	 
+     
+    });
+
+	
+
